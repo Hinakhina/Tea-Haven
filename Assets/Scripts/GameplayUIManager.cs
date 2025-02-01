38f1e6400cbe539f1651835348ef88bc7b012122
Vete <vincent.tanaka@binus.ac.id>
2025-02-01 13:36:02 +0700

    added teabrewing script

--- Assets/Scripts/GameplayUIManager.cs
+++ Assets/Scripts/GameplayUIManager.cs
@@ -1,10 +1,30 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class GameplayUIManager : MonoBehaviour
 {
+    public static GameplayUIManager Instance { get; private set; }
+
     [SerializeField] private TMP_Text statusText;
     [SerializeField] private GameObject teaBrewingPanel;
+    [SerializeField] private TMP_Text messageText;
+    [SerializeField] private List<string> availableIngredients;
+    private List<string> selectedIngredients = new List<string>();
+    private string currentOrder;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
 
     public void UpdateStatusText(string message)
     {
@@ -23,11 +43,34 @@ public class GameplayUIManager : MonoBehaviour
         }
     }
 
+    private bool IsCorrectOrder()
+    {
+        return selectedIngredients.Count > 0 && string.Join(", ", selectedIngredients) == currentOrder;
+    }
+
     public void HideTeaBrewingPanel()
     {
         if (teaBrewingPanel != null)
         {
             teaBrewingPanel.SetActive(false);
+        }
+    }
+
+    public void ShowSuccessMessage(string message)
+    {
+        if (messageText != null)
+        {
+            messageText.text = message;
+            messageText.color = Color.green;
+        }
+    }
+
+    public void ShowErrorMessage(string message)
+    {
+        if (messageText != null)
+        {
+            messageText.text = message;
+            messageText.color = Color.red;
         }
     }
 }
